27aa6c4b9f361e351ded4ce9443b46a55cb73f06
a2thedev <anthonygreen048@gmail.com>
2023-12-11 11:49:14 -0500

    added MusicTable component

---
+++ src/components/MusicTable/MusicTable.jsx
@@ -0,0 +1,12 @@
+const MusicTable = ({}) => {
+  const music = ["Always Knew", "Facts", "Broke", "Blessings"];
+  const musicItems = music.map((song) => <div>{song}</div>);
+  return (
+    <div>
+      <h4>My Music</h4>
+      <div>{musicItems}</div>
+    </div>
+  );
+};
+
+export default MusicTable;
